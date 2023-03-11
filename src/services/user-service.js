073eeea5389c61a0235245cdd93d51221b58a263
hazemax007 <hazem.bensalem@esprit.tn>
2023-03-11 01:26:56 +0100

    Second committ231

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'hhttps://node-js-jwt-authentication-qu6fwlqkh-hazemax007.vercel.app/api/test/';
+const API_URL = 'https://node-js-jwt-authentication-qu6fwlqkh-hazemax007.vercel.app/api/test/';
 
 class UserService {
   getPublicContent() {
